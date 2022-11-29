83f7b29901beaf5ac1fc5bf3d65260b2770efc88
gushch75 <102511005+gushch75@users.noreply.github.com>
2022-11-29 10:31:54 +0300

    Delete main2.js

--- Web-seminar/JavaScript/main2.js
+++
@@ -1,6 +0,0 @@
-function hello_user(user_name) {
-   // alert('Привет,${user_name }');
-    alert(`Тебе ${user_name} лет!`);
-}
-const user_name = prompt("Введите свое имя");
-hello_user(user_name);
